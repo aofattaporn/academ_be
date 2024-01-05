873a1c1bb18fd7b952a5dba77e8e3f8ffe4e753a
aofattaporn <aofattapon321@gmail.com>
2024-01-05 16:59:48 +0700

    auth: add new route for auth

--- models/user_models.go
+++ models/user_models.go
@@ -5,5 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type User struct {
 	Id       primitive.ObjectID `bson:"_id"`
 	Email    string             `json:"email,omitempty" validate:"required"`
-	Password string             `json:"password,omitempty" validate:"required"`
+	FullName string             `json:"fullName,omitempty" validate:"required"`
 }
