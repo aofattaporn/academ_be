1e8cbc0cddd50312cad4dccb060ff67542917715
aofattaporn <aofattapon321@gmail.com>
2024-01-05 16:59:23 +0700

    auth: set constant on package controllers

---
+++ controllers/constant.go
@@ -0,0 +1,17 @@
+package controllers
+
+const (
+
+	// Response Status constant
+	SUCCESS             string = "Success"
+	USER_SIGNIN_SUCCESS string = "User Sign In Sccuess"
+	USER_SIGNUP_SUCCESS string = "User Sign Up Sccuess"
+
+	ERROR               string = "Error"
+	EMAIL_PASSWORD_NULL string = "Email or Password is null"
+	INPUT_INVALID       string = "Input is invalid"
+	MONGO_ERROR         string = "Something wrong on mongoDB"
+
+	// validation constant
+	TOKEN string = "token"
+)
