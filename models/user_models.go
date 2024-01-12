5dd7be2984ba44615d5c60ddaa98d43a6badb7b6
aofattaporn <aofattapon321@gmail.com>
2024-01-12 19:35:12 +0700

    auth: change user model id map with UID firebase admin

--- models/user_models.go
+++ models/user_models.go
@@ -1,9 +1,7 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type User struct {
-	Id       primitive.ObjectID `bson:"_id"`
-	Email    string             `json:"email,omitempty" validate:"required"`
-	FullName string             `json:"fullName,omitempty" validate:"required"`
+	Id       string `bson:"_id"`
+	Email    string `json:"email,omitempty" validate:"required"`
+	FullName string `json:"fullName,omitempty" validate:"required"`
 }
