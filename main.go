501a9c9ebf1a05e7611a6e2e78efc93f6405cb38
aofattaporn <aofattapon321@gmail.com>
2024-02-25 18:06:31 +0700

    add swagger

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 package main
 
 import (
@@ -7,6 +26,8 @@ import (
 
 	"firebase.google.com/go/auth"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 var firebaseClient *auth.Client = configs.ConnectFirebase()
@@ -16,16 +37,17 @@ func main() {
 
 	// Run database
 	configs.ConnectDB()
-	admin := configs.ConnectFirebase()
+	// admin := configs.ConnectFirebase()
 
 	// Middlewares
 	router.Use(middlewares.CORSMiddleware())
-	router.Use(middlewares.AuthRequire(admin))
+	// router.Use(middlewares.AuthRequire(admin))
 	router.Use(middlewares.ErrorHandler())
 
 	// Routes
 	routes.UserRoute(router)
 	routes.ProjectRoute(router)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.Run("0.0.0.0:8080")
 }
