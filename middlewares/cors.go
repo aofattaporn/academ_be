7216f85a723ca87532c41610e8d4bdc06d8db6f1
aofattaporn <aofattapon321@gmail.com>
2024-05-15 20:19:39 +0700

    Header: checking Fcm_toekn

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -7,7 +7,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		origin := c.GetHeader("Origin")
 		c.Header("Access-Control-Allow-Origin", origin)
 		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, FCM_TOEKN")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, FCM_TOEKN, Fcm_toekn")
 		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
