cf17bdd3765d35b50c3f84567bf8024b5433811c
aofattaporn <aofattapon321@gmail.com>
2024-05-16 22:47:19 +0700

    noti: change noti body capitalize

--- models/notification_model.go
+++ models/notification_model.go
@@ -16,7 +16,7 @@ type NotificationRes struct {
 	ProjectProfile ProjectProfile `bson:"projectProfile" json:"projectProfile"`
 	UserId         string         `bson:"userId" json:"userId"`
 	Title          string         `bson:"title" json:"title"`
-	Body           string         `bson:"Body" json:"Body"`
+	Body           string         `bson:"Body" json:"body"`
 	Date           *time.Time     `bson:"date" json:"date"`
 	IsClear        bool           `bson:"isClear" json:"isClear"`
 }
